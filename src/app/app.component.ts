a007ee22c10fc8a2f0b5cb30e8186e76b06834e9
gonza <gonzazemont@gmail.com>
2024-08-04 18:55:40 -0400

    calculator v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'calculadora-angular';
+  num1 = '';
+  num2 = '';
+  operador: string = '';
+  resultado: number = 0;
+
+  calcular() {
+    switch (this.operador) {
+      case '+':
+        this.resultado = Number(this.num1) + Number(this.num2);
+        break;
+      case '-':
+        this.resultado = Number(this.num1) - Number(this.num2);
+        break;
+      case '*':
+        this.resultado = Number(this.num1) * Number(this.num2);
+        break;
+      case '/':
+        this.resultado = Number(this.num1) / Number(this.num2);
+        break;
+    }
+  }
 }
